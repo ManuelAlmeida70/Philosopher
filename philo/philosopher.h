dcf70e552bd7858b7e8b675247b91c86e0dd1e20
Manuel Mazunga De Almeida <maalmeid@c1r1s8.42luanda.com>
2024-09-22 20:06:34 +0100

    First commit, only philosopher.h included

---
+++ philo/philosopher.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosopher.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maalmeid <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/22 18:58:27 by maalmeid          #+#    #+#             */
+/*   Updated: 2024/09/22 19:03:36 by maalmeid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHER_H
+# define PHILOSOPHER_H
+
+# include <stdio.h>
+# include <string.h>
+# include <pthread.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/time.h>
+
+#endif
