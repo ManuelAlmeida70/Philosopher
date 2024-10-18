96860d4c12ef1ac9b9c8c0b64e8f77d877a93382
Manuel Mazunga De Almeida <maalmeid@c1r6s1.42luanda.com>
2024-10-18 18:18:16 +0100

    Norminette error at main.c and safe_function.c removed

--- philo/main.c
+++ philo/main.c
@@ -1,18 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maalmeid <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/18 18:09:02 by maalmeid          #+#    #+#             */
+/*   Updated: 2024/10/18 18:16:53 by maalmeid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <philo.h>
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    t_table table;
+	t_table	table;
 
-     if (ac ==  5 || ac == 6)
-     {
-        parse_input(&table, av);
-        data_init(&table);
-        dinner_start(&table);
-        clean(&table);
-     }
-     else
-     {
-        error_exit("Wrong input:\n"GREEN"Correct is ./philo 5 800 200 200 [5]"RST);
-     }
+	if (ac == 5 || ac == 6)
+	{
+		parse_input(&table, av);
+		data_init(&table);
+		dinner_start(&table);
+		clean(&table);
+	}
+	else
+	{
+		error_exit(GREEN"Correct is ./philo 5 800 200 200 [5]"RST);
+	}
+	return (0);
 }
